c5d753b8b24d21212e4dd0a3f39f27ea4d11739d
dragon2462 <121273332+dragon2462@users.noreply.github.com>
2023-05-01 11:44:21 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+// Get all the collapsible tabs
+var collapsibleTabs = document.querySelectorAll(".collapsible-tab");
+
+// Loop through all the collapsible tabs
+for (var i = 0; i < collapsibleTabs.length; i++) {
+  // Add a click event listener to each tab's button
+  collapsibleTabs[i].querySelector(".collapsible-button").addEventListener("click", function() {
+    // Toggle the 'active' class on the button when clicked
+    this.classList.toggle("active");
+    // Get the content associated with the button
+    var content = this.nextElementSibling;
+    // Toggle the 'show' class on the content
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+}
 
